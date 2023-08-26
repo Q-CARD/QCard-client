7d3f084d5a672e7ea3b1ecd2b6bc05cfe3641ed2
hyosin <cdnnnl@gmail.com>
2023-08-27 04:48:09 +0900

    chore: 푸터 마크업 WIP (#3)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,12 @@
-export function Footer() {
-    return <div>Footer</div>;
+export default function Footer() {
+    // aria-label: 이미지의 대체 텍스트 역할
+    // aria-labelledby: 화면에 현재 요소를 설명할 텍스트가 있는 경우 텍스트(id)와 현재 요소 연결
+    // 링크: https://abcdqbbq.tistory.com/77
+
+    return (
+        <footer className="w-full h-[16.2rem] px-[10rem] py-[6.2rem] bg-slate-100">
+            <section id="footer-content">footer</section>
+            <section id="footer-link">icon group</section>
+        </footer>
+    );
 }
