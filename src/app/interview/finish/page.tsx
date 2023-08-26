aa8b0caecd1b07346c21bae55cd7166623571261
hyosin <cdnnnl@gmail.com>
2023-08-27 04:47:37 +0900

    feat: 모의면접 레이아웃 작업 WIP (#9)

--- src/app/interview/finish/page.tsx
+++ src/app/interview/finish/page.tsx
@@ -1,4 +1,30 @@
 // 모의 면접 모드 종료 페이지 interview/finish
+import type { Metadata } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import ImgCelebrate from '@/assets/images/image-celebrate.png';
+
+export const metadata: Metadata = {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL),
+    title: 'Interview Finish',
+    description: 'Finish interview',
+};
+
 export default function InterviewFinishPage() {
-    return <div>InterviewFinishPage</div>;
+    return (
+        <section className="flex flex-col items-center m-auto pt-[10.2rem] pb-[11.8rem]">
+            <h1>모의 면접이 끝났습니다</h1>
+            <h1>__님의 성장을 기대할게요!</h1>
+            <Image
+                src={ImgCelebrate}
+                alt="image-celebrate"
+                width={995}
+                height={498}
+                loading="lazy"
+            />
+            <button>
+                <Link href="/">메인으로</Link>
+            </button>
+        </section>
+    );
 }
---
+++ src/constants/data.tsx
@@ -0,0 +1,30 @@
+export const QUESTION_CATEGORY = [
+    {
+        name: '네트워크',
+        id: 1,
+    },
+    {
+        name: '운영체제',
+        id: 2,
+    },
+    {
+        name: '데이터베이스',
+        id: 3,
+    },
+    {
+        name: 'JAVA',
+        id: 4,
+    },
+    {
+        name: 'Spring',
+        id: 5,
+    },
+    {
+        name: '컴퓨터구조',
+        id: 6,
+    },
+    {
+        name: '프론트엔드',
+        id: 7,
+    },
+];
