a2a76b3a2069e4bbe4b412a9b041339ff8a2d506
hyosin <cdnnnl@gmail.com>
2023-08-28 21:27:45 +0900

    feat: 체크박스 컴포넌트 추가 (#6)

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,3 +1,57 @@
-export default function Checkbox({ ...props }) {
-    return <div>Checkbox</div>;
+'use client';
+// 클라이언트 컴포넌트
+import React from 'react';
+
+interface CheckboxProps {
+    children: string;
+    handleCategoryList: (name: string) => void;
+}
+export default function Checkbox({
+    children,
+    handleCategoryList,
+}: CheckboxProps) {
+    // 체크 상태에 따라 테두리 여부 토글
+    const [isChecked, setIsChecked] = React.useState(false);
+
+    // TODO: 이벤트 타입 챙기기
+    const handleCheckboxClick = (e: any) => {
+        // name: any, e: any
+        const name = e.target.name;
+        const tagName = e.target.tagName;
+
+        if (tagName === 'LABEL') return;
+        setIsChecked(!isChecked);
+
+        if (tagName === 'DIV') {
+            handleCategoryList(children);
+            return;
+        }
+        handleCategoryList(name);
+    };
+
+    return (
+        <div
+            onClick={handleCheckboxClick}
+            className={`flex items-center rounded-3xl border p-[2.4rem] ${
+                isChecked ? 'border-blue-primary' : 'border-grey-4'
+            } cursor-pointer`}
+        >
+            <label
+                htmlFor={children}
+                className={`flex w-[35rem] text-base font-normal items-center ${
+                    isChecked ? 'text-blue-primary' : 'text-grey-4'
+                } cursor-pointer`}
+            >
+                {children}
+            </label>
+            <input
+                className="cursor-pointer"
+                id={children}
+                name={children}
+                checked={isChecked}
+                type="checkbox"
+                readOnly
+            />
+        </div>
+    );
 }
