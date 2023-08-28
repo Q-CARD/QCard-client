9cc85e67023d21694f94ede3f2187a771bc81406
hyosin <cdnnnl@gmail.com>
2023-08-28 21:33:23 +0900

    feat: 모의면접 질문들 라우팅 (#9)

---
+++ src/app/interview/[interviewid]/layout.tsx
@@ -0,0 +1,14 @@
+interface InterviewLayoutProps {
+    children: React.ReactNode;
+}
+
+export default function InterviewLayoutProps({
+    children,
+}: InterviewLayoutProps) {
+    return (
+        <section>
+            <h1 className="text-heading2 px-[10rem] py-[3.5rem]">모의 면접</h1>
+            <div className="flex flex-col mt-[8rem] m-auto">{children}</div>
+        </section>
+    );
+}
