079bdee788eacd8834f02f50d260b8065675df77
hyosin <cdnnnl@gmail.com>
2023-08-30 02:44:09 +0900

    feat: 모의면접 카테고리 선택 최대 3개로 제한 (#6)

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,42 +3,46 @@ import React from 'react';
 
 interface CheckboxProps {
     children: string;
-    handleCategoryList: (name: string) => void;
+    handleCategoryList: (name: string, isChecked: boolean) => void;
+    isCheckableNum: number; // 체크한 개수
 }
 
 export default function Checkbox({
     children,
     handleCategoryList,
+    isCheckableNum,
 }: CheckboxProps) {
     // 체크 상태에 따라 테두리 여부 토글
     const [isChecked, setIsChecked] = React.useState(false);
 
     // TODO: 이벤트 타입 챙기기
     const handleCheckboxClick = (e: any) => {
-        // name: any, e: any
         const name = e.target.name;
         const tagName = e.target.tagName;
 
         if (tagName === 'LABEL') return;
-        setIsChecked(!isChecked);
+
+        // 체크한 개수가 3개보다 적거나, 체크한게 3개인데 체크가 이미 되어 있는 상태라면 (체크 해제하는 상태라면)
+        if (isCheckableNum < 3 || (isCheckableNum === 3 && isChecked))
+            setIsChecked(!isChecked);
 
         if (tagName === 'DIV') {
-            handleCategoryList(children);
+            handleCategoryList(children, isChecked);
             return;
         }
-        handleCategoryList(name);
+        handleCategoryList(name, isChecked);
     };
 
     return (
         <div
             onClick={handleCheckboxClick}
-            className={`flex items-center rounded-3xl border p-[2.4rem] ${
+            className={`w-[30vw] max-w-[41rem] flex items-center rounded-3xl border p-[2.4rem] ${
                 isChecked ? 'border-blue-primary' : 'border-grey-4'
             } cursor-pointer`}
         >
             <label
                 htmlFor={children}
-                className={`flex w-[35rem] text-base font-normal items-center ${
+                className={`flex flex-1 text-base font-normal items-center ${
                     isChecked ? 'text-blue-primary' : 'text-grey-4'
                 } cursor-pointer`}
             >
