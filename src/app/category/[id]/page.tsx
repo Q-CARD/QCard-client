50430598b7afe1564120aedd0af9ab451ad08951
권가은 <k.gaeun1007@gmail.com>
2023-09-02 16:11:08 +0900

    rename: category 라우팅 수정

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,4 +1,4 @@
-// 질문 상세 페이지_입력 전
+// 질문 상세 페이지_1
 export default function CategoryDetailPage() {
     return <div>CategoryDetailPage</div>;
 }
---
+++ src/app/category/question/[id]/page.tsx
@@ -0,0 +1,4 @@
+// 질문 상세 페이지_입력전
+export default function CategoryQuestionPage() {
+    return <div>CategoryQuestionPage</div>;
+}
