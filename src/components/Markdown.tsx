bcf48c888cb9c01fc7806c08bcca993c3082c0bf
hyosin <cdnnnl@gmail.com>
2023-09-03 05:24:36 +0900

    fix: Markdown Renderer 서버 컴포넌트로 수정 (#17)

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,27 @@
+import { getInterviewAll } from '@/api/interview';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+
+// 커스텀 설정
+const components = {
+    h1: (props: any) => (
+        <h1 {...props} style={{ 'font-size': '25px' }} className="large-text">
+            {props.children}
+        </h1>
+    ),
+};
+
+export default async function MarkdownRenderer({
+    content,
+}: {
+    content: string;
+}) {
+    const source = 'Some **mdx** text, with a component';
+
+    const data = await getInterviewAll(5);
+    //console.log('data', data);
+    if (data) {
+        //  console.log('mdx', data);
+    }
+
+    return <MDXRemote components={{ ...components }} source={content} />;
+}
