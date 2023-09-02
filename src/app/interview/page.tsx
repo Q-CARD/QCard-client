000fe557728acb31eefdd4d9d814a4b5b1326718
hyosin <cdnnnl@gmail.com>
2023-09-03 05:54:59 +0900

    feat: 인터뷰 시작 api 연결

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -9,10 +9,21 @@ import Checkbox from '@/components/Checkbox';
 import ImgCardDeck2 from '@/assets/images/image-card-deck-2.png';
 import { QUESTION_CATEGORY } from '@/constants/data';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import { newInterview } from '@/api/interview';
+import { useSetRecoilState } from 'recoil';
+import {
+    categoryListAtom,
+    interviewListAtom,
+    interviewIdAtom,
+} from '@/utils/atom';
 
 export default function InterviewPage() {
     // TODO: 질문 유형 데이터 api 호출
     const [categoryList, setCategoryList] = React.useState<string[]>([]);
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
 
     const router = useRouter();
 
@@ -30,14 +41,37 @@ export default function InterviewPage() {
         }
     };
 
-    const handleInterviewStart = () => {
-        // TODO: interviewId 획득
-        const interviewid = 1;
+    const setCategoryListAtom = useSetRecoilState(categoryListAtom);
+    const setInterviewListAtom = useSetRecoilState(interviewListAtom);
+    const setInterviewIdAtom = useSetRecoilState(interviewIdAtom);
+
+    const handleInterviewStart = async () => {
+        let interviewid = 1;
+
+        let body = categoryList.map(
+            (category) =>
+                QUESTION_CATEGORY.find(
+                    (question: any) => question.name === category,
+                )?.key,
+        );
+
+        // 체크 리스트 Recoil에 저장
+        setCategoryListAtom(body);
+
+        if (!body || body.length === 0) return;
+
+        let data = await newInterview(body as string[]);
+        if (data) {
+            console.log('data', data);
+            interviewid = data.interview_id;
+            setInterviewListAtom(data.question); // response 저장
+            setInterviewIdAtom(data.id); // interview_question_id 저장
+        }
+
         const pathname = `/interview/${interviewid}?question=1`;
-        router.push(pathname);
+        router.replace(pathname);
     };
 
-    //   <section className="flex justify-center pt-[17rem] min-w-[86rem] mr-[19.3rem] pb-[17.3rem] ml-[14.4rem]">
     return (
         <section className="flex justify-center pt-[17rem] min-w-[86rem] m-auto pb-[17.3rem] ">
             <div>
