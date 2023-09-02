256a1f558e7d9517cc96bbb59ec6d7590fcaa7ee
hyosin <cdnnnl@gmail.com>
2023-09-03 05:55:24 +0900

    chore: RecoilRoot 추가

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,13 +1,17 @@
 'use client';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { MDXProvider } from '@mdx-js/react';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     const [queryClient] = React.useState(() => new QueryClient());
 
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <MDXProvider>{children}</MDXProvider>
+            </QueryClientProvider>
+        </RecoilRoot>
     );
 }
