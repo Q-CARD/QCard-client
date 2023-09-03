fc95588a16a9ddf58175c0bd67412cbde8f90c25
hyosin <cdnnnl@gmail.com>
2023-09-03 12:08:02 +0900

    chore: 불필요한 파일 제거 (#30)

--- src/hooks/useInterview.ts
+++
@@ -1,8 +0,0 @@
-import { useRecoilValue } from 'recoil';
-import { interviewIdAtom } from '@/utils/atom';
-
-export const useInterview = (num: number) => {
-    const obj = useRecoilValue(interviewIdAtom);
-    console.log('[obj]', obj); // [ empty, 22 ,23, 24, ...]
-    return obj[num];
-};
