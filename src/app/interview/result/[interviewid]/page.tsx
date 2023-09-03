0c3963306c7d2c4db18d66d4f8fce31f8443b1f4
hyosin <cdnnnl@gmail.com>
2023-09-03 17:22:01 +0900

    style: 질문 타이틀 word-break: keep-all 적용

--- src/app/interview/result/[interviewid]/page.tsx
+++ src/app/interview/result/[interviewid]/page.tsx
@@ -8,7 +8,7 @@ import MarkdownRenderer from '@/components/Markdown';
 import NextButton from '@/components/interview/NextButton';
 import FollowupButton from '@/components/interview/FollowupButton';
 import Question from '@/components/interview/Question';
-
+import QuestionSkeleton from '@/components/interview/QuestionSkeleton';
 import { getInterviewAll } from '@/api/interview';
 
 export default async function InterviewResultPage() {
@@ -18,29 +18,42 @@ export default async function InterviewResultPage() {
     const header_url_fullUrl = headerList.get('referer') || '';
 
     // 서버 컴포넌트여서 useParams 사용 불가 => headerList.referer로 요청 URL 접근
+
     const interview_question_id = header_url_fullUrl
         .split('=')?.[1]
         ?.split('&')?.[0];
 
-    let data = await getInterviewAll(parseInt(interview_question_id));
+    // 2번 호출이 됨 , 첫째는 로드밸런서, 둘째는 30way handshake
+    console.log('header_url_fullUrl', header_url_fullUrl);
+
+    let data;
+    if (header_url_fullUrl.includes('result')) {
+        data = await getInterviewAll(parseInt(interview_question_id));
+        console.log(
+            'interview_question_id',
+            interview_question_id,
+            'data',
+            data,
+        );
+    }
 
     return (
         <>
             <div className="flex text-center flex-col w-[85rem] items-center rounded-2xl border border-grey-4 py-[4rem] px-[4.8rem] gap-[3.2rem] self-stretch">
+                <Suspense fallback={<QuestionSkeleton/>}>
                 <Question />
-                <Suspense fallback={<p>Loading feed...</p>}>
+                </Suspense>
                     <Accordian
                         rerenderProps={interview_question_id}
                         className="w-full"
                     >
                         <MarkdownRenderer
                             content={
-                                data.gpt_answer ??
+                                data?.gpt_answer ??
                                 'gpt 답변이 없습니다 다시 한번 녹음해주세요 :)'
                             }
                         />
                     </Accordian>
-                </Suspense>
                 <FollowupButton />
             </div>
             <Pagination className="mt-[3.4rem]" />
