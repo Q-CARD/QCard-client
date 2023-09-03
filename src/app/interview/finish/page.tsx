cfa72ff04a080d71fd970b70ce92580c808cb955
hyosin <cdnnnl@gmail.com>
2023-09-03 19:14:59 +0900

    chore: 모의면접 종료 > 유저 닉네임 추가 (#37)

--- src/app/interview/finish/page.tsx
+++ src/app/interview/finish/page.tsx
@@ -1,23 +1,37 @@
+'use client'
 // 모의 면접 모드 종료 페이지 interview/finish
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 import Image from 'next/image';
+import {useState, useEffect} from 'react'
 import Link from 'next/link';
 import ImgCelebrate from '@/assets/images/image-celebrate.png';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from '@/store/recoil';
 
-export const metadata: Metadata = {
-    metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
-    title: 'Interview Finish',
-    description: 'Finish interview',
-};
+//export const metadata: Metadata = {
+//    metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+//    title: 'Interview Finish',
+//    description: 'Finish interview',
+//};
 
 export default function InterviewFinishPage() {
+
+    const user = useRecoilValue(userAtom)
+    const [nickname, setNickname] = useState()
+
+    useEffect(() => {
+        setNickname(user.nickname ?? '유저')
+    }, [])
+
+    
+
     return (
         <section className="flex flex-col items-center m-auto pt-[10.2rem] pb-[11.8rem]">
             <h1 className="text-black text-heading2">
                 모의 면접이 끝났습니다
                 <br />
-                __님의 성장을 기대할게요!
+                {nickname}님의 성장을 기대할게요!
             </h1>
             <Image
                 src={ImgCelebrate}
