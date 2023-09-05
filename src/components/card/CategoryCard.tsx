71bdac27f15e23a7fbe5bde054af12ec0f89a56b
hyosin <cdnnnl@gmail.com>
2023-09-06 02:32:48 +0900

    feat: 카테고리 질문 진입 전 로그인 검사 (#37)

--- src/components/card/CategoryCard.tsx
+++ src/components/card/CategoryCard.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-
+import { useRecoilValue } from 'recoil';
+import { isLoginAtom } from '@/store/recoil';
 import { ICategory } from '@/types';
 
 interface CategoryCardProps {
@@ -11,15 +12,22 @@ export function CategoryCard({ categoryInfo }: CategoryCardProps) {
     const { id, name, image } = categoryInfo;
 
     const router = useRouter();
+    const isLogin = useRecoilValue(isLoginAtom);
 
-    const moveToCategoryQuestion = () => {
-        router.push(`/category/${id}`);
+    // path 진입 전 로그인 여부 검사
+    const beforeEnter = (path: string) => {
+        if (isLogin) {
+            router.push(path);
+        } else {
+            alert('로그인이 필요한 기능입니다');
+            router.push('/auth/login');
+        }
     };
 
     return (
         <div
-            className="w-fit h-fit px-[4.8rem] py-[6.6rem] rounded-[2rem] shadow-cardShadow cursor-pointer flex flex-col items-center gap-[3.7rem]"
-            onClick={moveToCategoryQuestion}
+            className="w-fit h-fit px-[4.8rem] py-[6.6rem] rounded-[2rem] shadow-4 cursor-pointer flex flex-col items-center gap-[3.7rem]"
+            onClick={() => beforeEnter(`/category/${id}`)}
         >
             <Image
                 src={image}
