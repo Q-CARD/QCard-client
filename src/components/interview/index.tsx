5d4659b0a1c9e05fddd613f568ca697de9d5547d
hyosin <cdnnnl@gmail.com>
2023-09-11 15:34:09 +0900

    chore: interview 컴포넌트 index.js export로 수정

---
+++ src/components/interview/index.tsx
@@ -0,0 +1,4 @@
+export { default as FollowupButton } from './FollowupButton';
+export { default as NextButton } from './NextButton';
+export { default as Question } from './Question';
+export { default as QuestionSkeleton } from './QuestionSkeleton';
