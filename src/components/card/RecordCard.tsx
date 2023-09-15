460a0b5d377b1a3da429904b59bf0c2496273aa6
hyosin <cdnnnl@gmail.com>
2023-09-15 14:12:37 +0900

    feat: 모의면접 > 최대 녹음 시간 10분으로 변경 (#60)

--- src/components/card/RecordCard.tsx
+++ src/components/card/RecordCard.tsx
@@ -1,14 +1,7 @@
-'use client';
-
 import React from 'react';
 import Timer from '../Timer';
 import Recoder from '../Recoder';
-
-interface QuestionType {
-    category: string; // "CATEGORY_NW"
-    id: number; // question_id
-    title: string;
-}
+import { IQuestionInterview } from '@/types';
 
 export default function RecordCard({
     question,
@@ -17,27 +10,38 @@ export default function RecordCard({
     getRecordingStatus,
 }: {
     interviewQuestionId: number;
-    question: QuestionType | undefined;
+    question: IQuestionInterview | undefined;
     cnt: number;
     getRecordingStatus: (isRecording: boolean | null) => void;
 }) {
-    // 타이머 클릭 & 녹음 stop/start 상태 관리
     const [isRecording, setIsRecording] = React.useState<null | boolean>(null);
+    const timeRef = React.useRef<number>(0);
+
+    const MIN_TIME_LIMIT = 60;
 
     React.useEffect(() => {
         getRecordingStatus(isRecording);
     }, [isRecording]);
-    // 시작
+
     const handleRecordStart = () => {
-        //console.log('handleRecordStart');
         setIsRecording(true);
     };
 
-    // 중단
-    const handleRecordStop = () => {
-        if (isRecording) {
+    const getTime = (time: number) => {
+        if (timeRef) {
+            timeRef.current = time;
         }
-        //console.log('handleRecordStop');
+    };
+
+    /**
+     * @description 녹음 중지 버튼 클릭 시 호출 & isRecording 상태 관리
+     * - 순서: onClick -> handleRecordStop 호출 -> isRecording 상태 변화 -> Timer start/stop
+     */
+    const handleRecordStop = () => {
+        if (timeRef && timeRef.current < MIN_TIME_LIMIT) {
+            alert('최소 1분은 녹음해주세요');
+            return;
+        }
         setIsRecording(false);
     };
 
@@ -50,9 +54,12 @@ export default function RecordCard({
         <div className="flex flex-col items-center rounded-2xl border border-grey-4 py-[3.2rem] px-[2.4rem] gap-[5.4rem] self-stretch">
             <div className="text-heading3 text-blue-primary">{cnt}</div>
             <div className="text-center flex items-center flex-col m-auto">
-                <h1 className="flex text-specialHeading w-3/5 break-keep text-center">{question?.title}</h1>
+                <h1 className="flex text-specialHeading w-3/5 break-keep text-center">
+                    {question?.title}
+                </h1>
                 <div className="text-bodyDefault text-grey-5 mt-[2rem]">
-                    녹음한 대답을 기반으로 꼬리질문을 드립니다<br/>
+                    녹음한 대답을 기반으로 꼬리질문을 드립니다
+                    <br />
                     답변 시간은 1분입니다
                 </div>
             </div>
@@ -67,7 +74,8 @@ export default function RecordCard({
                     handleRecordStart={handleRecordStart}
                     handleRecordStop={handleRecordStop}
                     isRecording={isRecording}
-                    mm={1}
+                    getTime={getTime}
+                    mm={10}
                     ss={0}
                 />
             </div>
