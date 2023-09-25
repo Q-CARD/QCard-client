0aa4230d8a17a90fd5d32a870656d2a7bd015ea8
권가은 <k.gaeun1007@gmail.com>
2023-09-25 18:53:35 +0900

    chore: wretch 라이브러리 추가, axios 삭제

--- src/api/interview.ts
+++ src/api/interview.ts
@@ -1,14 +1,14 @@
-import { GET, POST, PUT } from '@/utils/axios';
+import { http } from '@/utils/fetch';
 import { IAnswerFollwupQuestion } from '@/types';
 
 // 면접보길 원하는 카테고리 리스트 전송받은 후 인터뷰 id 반환
 export const newInterview = async (categoryList: string[]) => {
-    return await POST(`/interview/start`, { category: categoryList });
+    return await http.POST(`/interview/start`, { category: categoryList });
 };
 
 // 모의면접의 interview_question_id 당 모든 값을 반환
 export const getInterviewAll = async (interviewQuestionId: number) => {
-    return await GET(`/interview/all/${interviewQuestionId}`);
+    return await http.GET(`/interview/all/${interviewQuestionId}`);
 };
 
 // 특정 모의면접에서 한가지 질문에 대한 답변 Mp3 파일 전송
@@ -16,12 +16,12 @@ export const submitRecordFile = async (
     interview_question_id: number,
     body: FormData,
 ) => {
-    return await PUT(`/interview/answer/${interview_question_id}`, body, {
+    return await http.PUT(`/interview/answer/${interview_question_id}`, body, {
         type: 'record',
     });
 };
 
 // 꼬리질문 답변 전송
 export const submitAdditionalAnswer = async (body: IAnswerFollwupQuestion) => {
-    return await PUT(`/interview/additional`, body);
+    return await http.PUT(`/interview/additional`, body);
 };
