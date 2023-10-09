3eca5f91dbb1d0f2d60b3d4fb45a9259b1fa0931
권가은 <k.gaeun1007@gmail.com>
2023-10-09 22:25:17 +0900

    feat: /category/result/[id] 디자인 & api 변경 반영

--- src/app/category/result/[id]/page.tsx
+++ src/app/category/result/[id]/page.tsx
@@ -1,15 +1,20 @@
-// 질문 상세 페이지_입력 후
 'use client';
 
 import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
-import { TextBoxWrapper } from '@/components/TextBoxWrapper';
-import { Profile } from '@/components/Profile';
 import { getQuestionById } from '@/api/questions';
 import { IAnswerHearted, IQuestion } from '@/types';
+import defaultImage from '@/assets/icons/icon-default-profile.png';
+import ThumbsUpIcon from '@/assets/icons/icon-thumbsup.png';
+import ThumbsUpFillIcon from '@/assets/icons/icon-thumbsup-fill.png';
+import Image from 'next/image';
+import { deleteHearts, postHearts } from '@/api/hearts';
 
+/**
+ * @description 질문모음 > 개별 질문 페이지 (입력 후)
+ */
 export default function CategoryResultPage({
     params,
 }: {
@@ -30,7 +35,7 @@ export default function CategoryResultPage({
     // TODO - question,answer 페이지 중복 호출 개선
     const loadQuestionDetail = async () => {
         try {
-            const data = await getQuestionById(Number(params.id));
+            const data = await getQuestionById(Number(params.id), 'SORT_HEART');
 
             const myAnswer = data.answers.find((answer: IAnswerHearted) => {
                 return answer.isMine;
@@ -50,39 +55,126 @@ export default function CategoryResultPage({
     };
 
     return (
-        <div className="my-[12.8rem] flex flex-col items-center gap-[3.2rem]">
-            <div className="text-specialHeading mb-[0.8rem]">
-                <span className="text-blue-primary">Q. </span>
-                <span>{questionInfo?.title}</span>
-            </div>
-            <TextBoxWrapper>{myAnswer?.content ?? ''}</TextBoxWrapper>
-
-            <div className="text-specialHeading mb-[0.8rem]">
-                <span className="text-yellow-sub">A. </span>
-                <span>GPT의 답변이에요</span>
-            </div>
-            <TextBoxWrapper>
-                <ReactMarkdown
-                    children={gptAnswer?.content ?? 'gpt 답변이 없습니다.'}
-                    remarkPlugins={[remarkGfm]}
-                />
-            </TextBoxWrapper>
-
-            <div className="text-specialHeading mb-[0.8rem]">
-                <span className="text-yellow-sub">A. </span>
-                <span>다른 사람들의 답변도 살펴볼까요?</span>
-            </div>
-            {otherAnswersList.map((answer: IAnswerHearted) => {
-                return (
-                    <div
-                        key={`other-answer-${answer.answerId}`}
-                        className="flex flex-col gap-[1.6rem]"
-                    >
-                        <Profile account={answer.account} />
-                        <TextBoxWrapper>{answer.content}</TextBoxWrapper>
+        <div className="my-[6.8rem] flex justify-center">
+            <div className="w-[70rem] flex flex-col gap-[9.5rem]">
+                <div className="flex flex-col gap-[5.2rem]">
+                    <div className="text-specialHeading mb-[0.8rem]">
+                        <span className="text-blue-primary">Q.&nbsp;</span>
+                        <span>{questionInfo?.title}</span>
                     </div>
-                );
-            })}
+                    <div className="h-fit p-[3.2rem] text-bodySmall text-black rounded-[1rem] border-[0.3rem] border-blue-3">
+                        {myAnswer?.content ?? '답변 없음'}
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-[5.2rem]">
+                    <div className="text-specialHeading mb-[0.8rem]">
+                        <span className="text-yellow-sub">A.&nbsp;</span>
+                        <span>GPT의 답을 확인해보세요</span>
+                    </div>
+                    <div className="h-fit p-[3.2rem] text-bodySmall text-black bg-yellow-1 rounded-[1rem] border-[0.3rem] border-yellow-3">
+                        <ReactMarkdown
+                            children={
+                                gptAnswer?.content ?? 'gpt 답변이 없습니다.'
+                            }
+                            remarkPlugins={[remarkGfm]}
+                        />
+                    </div>
+                </div>
+
+                <hr className="h-[0.2rem] bg-grey-4" />
+
+                <div className="flex flex-col gap-[5.2rem]">
+                    <div className="text-specialHeading mb-[0.8rem]">
+                        <span className="text-yellow-sub">A.&nbsp;</span>
+                        <span>동료들의 답변도 살펴보세요</span>
+                    </div>
+                    {otherAnswersList.map((answer: IAnswerHearted) => {
+                        return (
+                            <OtherAnswerCard
+                                key={`other-answer-${answer.answerId}`}
+                                data={answer}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function OtherAnswerCard({ data }: { data: any }) {
+    const {
+        answerId,
+        type,
+        account,
+        content,
+        heartCount,
+        createdAt,
+        modifiedAt,
+        isHearted,
+        isMine,
+    } = data;
+    return (
+        <div className="flex flex-col gap-[3rem]">
+            <div className="min-h-[7rem] flex items-center gap-[2.6rem]">
+                <div className="flex items-center gap-[1rem]">
+                    <div
+                        style={{
+                            display: 'flex',
+                            position: 'relative',
+                            width: '7rem',
+                            height: '7rem',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        <Image
+                            src={account.profile ?? defaultImage}
+                            alt="profile-image"
+                            fill
+                            sizes="7rem"
+                            style={{
+                                borderRadius: '50%',
+                                objectFit: 'cover',
+                            }}
+                        />
+                    </div>
+                    <span className="ml-[2.4rem] text-bodyDefault text-grey-6">
+                        {account.name}
+                    </span>
+                </div>
+                <div className="w-[0.2rem] h-[2.8rem] bg-grey-4"></div>
+                {isHearted ? (
+                    <Image
+                        src={ThumbsUpFillIcon}
+                        alt="thumbsupfill-icon"
+                        width={28}
+                        height={28}
+                        className="cursor-pointer"
+                        onClick={async () => {
+                            await postHearts(answerId);
+                        }}
+                    />
+                ) : (
+                    <Image
+                        src={ThumbsUpIcon}
+                        alt="thumbsup-icon"
+                        width={28}
+                        height={28}
+                        className="cursor-pointer"
+                        onClick={async () => {
+                            await deleteHearts(answerId);
+                        }}
+                    />
+                )}
+                <span className="text-grey-5 text-[1.8rem] font-semibold leading-[2.4rem]">
+                    {heartCount}
+                </span>
+            </div>
+
+            <div className="h-fit p-[3.2rem] text-bodySmall text-black bg-blue-1 rounded-[1rem] border-[0.3rem] border-blue-3">
+                {data.content}
+            </div>
         </div>
     );
 }
