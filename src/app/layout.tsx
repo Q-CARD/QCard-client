3be473c62aed8d6b8c6e4249ef98ee7f3ca77ae2
hyosin <cdnnnl@gmail.com>
2023-10-21 16:36:57 +0900

    feat: 구글 애드센스 메타 태그 설정 (#92)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,10 @@ export default function RootLayout({
                     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
                 />
                 <link rel="icon" href="./favicon.ico" />
+                <meta
+                    name="google-adsense-account"
+                    content="ca-pub-8512927500598229"
+                ></meta>
             </head>
             <body className="min-h-screen">
                 <Providers>
