00439c6c549b9ecf92a22a9da1b35f0fdd18cfb6
권가은 <k.gaeun1007@gmail.com>
2023-10-22 17:29:30 +0900

    fix: postPresignedURL, env 파일 변경

--- src/api/presigned.ts
+++ src/api/presigned.ts
@@ -1,16 +1,9 @@
 import wretch from 'wretch';
-import { CONSTANTS } from '@/constants/common';
-
-let token = null;
-if (typeof window !== 'undefined') {
-    token = localStorage.getItem(CONSTANTS.ACCESS_TOKEN);
-}
 
 const api = wretch(process.env.NEXT_PUBLIC_PRESIGNED_URL)
-    .auth(`${token}`)
     .errorType('json')
-    .resolve((r: any) => r.json() as any);
+    .resolve((r: any) => r.res() as any);
 
 export const postPresignedURL = async (payload: any) => {
-    return await api.url('/default/presigned').post(payload);
+    return await api.post(payload);
 };
