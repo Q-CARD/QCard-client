b88b1a05a73c81bf16872099454ca6903e03e03e
hyosin <cdnnnl@gmail.com>
2023-10-26 20:47:52 +0900

    refactor: 미들웨어 authenticated 판별 시 쿠키 이용 (WIP)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
+'use server';
 import { NextRequest, NextResponse, NextFetchEvent } from 'next/server';
-
+import { CONSTANTS } from './constants';
 // Limit the middleware to paths in matcher array.
 export const config = {
     matcher: ['/mypage/:path*', '/interview/:path*', '/category/:path*'],
@@ -7,6 +8,7 @@ export const config = {
 
 export function middleware(request: NextRequest) {
     // TODO - authenticated 판별 수정 -> cookie
+    const cookie = request.cookies.get(CONSTANTS.ACCESS_TOKEN);
     if (false) {
         return NextResponse.redirect(new URL('/auth/login', request.url));
     }
