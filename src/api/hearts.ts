a443d3089aaaef92e94be27670fd58310bcf4c55
hyosin <cdnnnl@gmail.com>
2023-11-11 20:19:23 +0900

    docs: api 호출하는 곳 주석 추가

--- src/api/hearts.ts
+++ src/api/hearts.ts
@@ -1,11 +1,11 @@
 import { http } from '@/utils/fetch';
 
-// 좋아요 누르기 (답변이 isHearted == false인 경우 호출)
+// 좋아요 누르기 (답변이 isHearted == false인 경우 호출) - 클라이언트
 export const postHearts = async (answerId: number) => {
     return await http.POST(`/hearts/${answerId}`);
 };
 
-// 좋아요 취소하기 (답변이 isHearted == true인 경우 호출)
+// 좋아요 취소하기 (답변이 isHearted == true인 경우 호출) - 클라이언트
 export const deleteHearts = async (answerId: number) => {
     return await http.DELETE(`/hearts/${answerId}`);
 };
