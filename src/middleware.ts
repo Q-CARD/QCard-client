55207846a4db25dc2b591e87bc9ec1006f85a380
hyosin <cdnnnl@gmail.com>
2023-11-11 20:29:40 +0900

    feat: 로그인 필요한 페이지 진입 전 로그인 여부 확인

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,6 +13,8 @@ export const config = {
         '/category/:path*',
     ],
 };
+
+// 로그인 필요한 페이지 진입 전 로그인 여부 확인
 export function middleware(request: NextRequest) {
     const cookie: string | undefined = request.cookies.get(
         CONSTANTS.ACCESS_TOKEN,
